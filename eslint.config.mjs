158100b601d0d3eb51a62b974c939d60d68bd789
Pavel Sokolov <pavel.sok77@gmail.com>
2026-06-18 20:58:50 +0300

    Add Allure reporting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,8 +3,20 @@ import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default [
+    {
+        ignores: [
+            'node_modules/**',
+            'playwright-report/**',
+            'allure-report/**',
+            'allure-results/**',
+            'test-results/**',
+        ],
+    },
+
     js.configs.recommended,
+
     ...tseslint.configs.recommended,
+
     {
         files: ['**/*.ts'],
         languageOptions: {
